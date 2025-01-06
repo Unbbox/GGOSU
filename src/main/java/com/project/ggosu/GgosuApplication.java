71dd4616c491b9b9dde4f587a6eb3c66596570d6
un8ox <88935686+Unbbox@users.noreply.github.com>
2025-01-07 00:37:11 +0900

    Add files via upload
    Start Project

---
+++ src/main/java/com/project/ggosu/GgosuApplication.java
@@ -0,0 +1,13 @@
+package com.project.ggosu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgosuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgosuApplication.class, args);
+	}
+
+}
